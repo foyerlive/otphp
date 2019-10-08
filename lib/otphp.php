e51e861eed55eeed0a4d5b09b71f4fa9a39474d9
Pramod Kumar <pramod.kumar@ie.com.au>
2019-10-08 17:15:42 +1100

    update package to use compose

--- lib/otphp.php
+++ lib/otphp.php
@@ -20,7 +20,7 @@
  * THE SOFTWARE.
  */
 
-require_once dirname(__FILE__).'/../vendor/libs.php';
+require_once dirname(__FILE__).'/base32.php';
 require_once dirname(__FILE__).'/otp.php';
 require_once dirname(__FILE__).'/hotp.php';
 require_once dirname(__FILE__).'/totp.php';
